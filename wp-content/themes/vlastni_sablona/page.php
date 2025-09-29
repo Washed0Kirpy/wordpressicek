6d87ed26b960bd4f724fe8513a8e54f35c3d13ff
Jan Kirpal <Jan.Kirpal@student.souepl.cz>
2025-09-29 10:40:02 +0200

    fjd

---
+++ wp-content/themes/vlastni_sablona/page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php  
+            if ( is_front_page() ) { // Pokud je to domovská stránka (statická stránka)
+                echo get_the_title( get_option( 'page_on_front' ) ); // Získá název statické domovské stránky
+            } else {
+                wp_title( '|', true, 'right' ); // Standardní titulek pro ostatní stránky
+            }
+         ?></title>
+    </head>
+    <body>
+        <?php
+            get_header();
+        ?>
+
+        <main id="primary" class="site-main">
+            <?php
+                the_content();
+            ?>
+        </main>
+
+        <?php
+            get_footer();
+        ?>
+    </body>
+</html>
